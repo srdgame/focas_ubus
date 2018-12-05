6243a4975ce6b35a00397b345b58a03b7487c914
Dirk Chang <dirk@kooiot.com>
2018-12-05 16:28:54 +0800

    fixed  time

--- src/focas_quit.c
+++ src/focas_quit.c
@@ -27,6 +27,9 @@ static int focas_quit(struct ubus_context *ctx, struct ubus_object *obj,
 	blobmsg_parse(policy_connect, __CONNECT_MAX, tb, blob_data(msg), blob_len(msg));
 	if (tb[FOCAS_QUIT_TIME])
 		quit_time = blobmsg_get_u32(tb[FOCAS_QUIT_TIME]);
+	if (quit_time <= 0) {
+		quit_time = 3;
+	}
 	
 
 	memset(&evp, 0, sizeof(struct sigevent));
@@ -42,7 +45,7 @@ static int focas_quit(struct ubus_context *ctx, struct ubus_object *obj,
  
 	it.it_interval.tv_sec = quit_time;
 	it.it_interval.tv_nsec = 0;
-	it.it_value.tv_sec = 1;		
+	it.it_value.tv_sec = quit_time;
 	it.it_value.tv_nsec = 0;
  
 	if (timer_settime(timerid, 0, &it, NULL) == -1)
